100d0a022753927f3183dbcc5ffe1b2a96a7f479
Matheus Matos <matheusmatos463463@gmail.com>
2026-02-24 22:12:06 -0300

    Primeira Aula JS

---
+++ Aula 1/src/script.js
@@ -0,0 +1,11 @@
+// Exemplo de variavel let
+let nome = "Matheus"
+let nome1 = "Richard"
+
+console.log("Aluno: ", nome, "\nProfessor: ", nome1);
+
+
+// Exemplo de variavel const e array
+const times = ["São Paulo", "Corinthians", "Santos"]
+
+console.log(times)
