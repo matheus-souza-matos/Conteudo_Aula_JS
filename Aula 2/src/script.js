f5a7e03424694029bcfa2d4c87d806570af7ad9b
Matheus Matos <matheusmatos463463@gmail.com>
2026-02-25 21:18:36 -0300

    Continuação de Variáveis

---
+++ Aula 2/src/script.js
@@ -0,0 +1,92 @@
+// Exemplo de Operador de Atribuição
+// let x = 0;
+// let operacao = x + 5;
+
+// console.log(operacao);
+
+// Exemplos de aritmética em JavaScript
+// let soma = 5 + 5 + 4;
+// console.log(soma)
+
+// let nomeCompleto = "Matheus" + " Matos"
+// console.log(nomeCompleto)
+
+
+
+//Exemplos de tipos em JavaScript
+//String
+let cor = "azul";
+let frruta = "Maçã";
+console.log(cor);
+console.log(frruta);
+
+//Soma
+let soma1 = 5 + 3;
+let subtracao = 5 - 3;
+console.log(soma1);
+console.log(subtracao);
+
+//BigInt
+let bigint = 1234567890123456789012345n;
+let bigint2 = BigInt(1234567890123456789012345);
+console.log(bigint);
+console.log(bigint2);
+
+//Boolean
+let valorVerdadeiro = true;
+let valorFalso = false;
+console.log(valorVerdadeiro);
+console.log(valorFalso);
+
+
+//Objeto
+const carro = {
+    marca: 'Ferrari',
+    modelo: 'Turbo',
+    cor: 'Vermelho',
+};
+console.log(carro);
+console.log(`A marca do carro é ${carro.marca}`);
+
+
+
+//Array
+const frutas = ["Uva", "Banana", "Maçã"];
+console.log(frutas);
+console.log(frutas[2]);
+
+
+//Date Object
+const data = new Date;
+console.log(data);
+
+
+//Undefined
+let indefinido;
+console.log(indefinido);
+
+//Null
+let nulo = null;
+console.log(nulo);
+
+
+//Synbol
+let y = Symbol();
+console.log(y);
+
+
+//Exemplos de Typeof
+let stringTipo = "300";
+let numberTipo = 300;
+const tipoDado = typeof stringTipo;
+const tipoDado2 = typeof numberTipo;
+console.log(tipoDado);
+console.log(tipoDado2);
+
+
+//Exemplos de Boolean
+let cinco = 5;
+let comparacao = cinco == 8;
+console.log(comparacao);
+
+
