169ab07024c393a0e2c35e88d5937b8e2a41d671
Matheus Matos <matheusmatos463463@gmail.com>
2026-02-25 22:07:39 -0300

    Operadores Aritméticos JS

---
+++ Aula 2/src/script02.js
@@ -0,0 +1,22 @@
+//Exemplos de Operações Aritméticas
+let a = 5;
+let x = (100 + 200) * a;
+console.log(x);
+
+
+//Exemplo de adição
+let text1 = "Somente a dor coletiva";
+text1 += "gera a união!";
+console.log(text1)
+
+
+//Exemplo de incremento
+let w = 5;
+w++;
+let z = w;
+console.log(z);
+
+let b = 5;
+b--;
+let c = b;
+console.log(b);
