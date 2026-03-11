e43996e830cd4b0bb75d9096c25cd1182d0c63bf
Matheus Matos <matheusmatos463463@gmail.com>
2026-03-11 19:13:48 -0300

    Atividade 9 e 10 atualizadas

---
+++ Aula 9/src/script03.js
@@ -0,0 +1,26 @@
+// Utilizando o BigInt
+let a = BigInt("1827349017230948710928374091283409182734091723490871203984710982374091827340981723094810928347091823740981273490817720398471");
+console.log(a);
+
+// Utilizando n
+let b = 19284109283740981237409817230984710923840172364987162837946190238471902834871236498712635981720398471082364781263841234n;
+console.log(b);
+
+// Utilizando operadores aritméticos com BigInt
+let c = 238479281347912498127923n;
+let d = 238479281347912498127923n;
+let e = (c * d);
+console.log(e);
+
+// Convertendo BigInt
+let f = BigInt("9128309812039810923801238081203");
+let fConvertido = Number(f);
+console.log(fConvertido);
+
+// Utilizando métodos de comparação em BigInt
+let g = 100n;
+let h = 90n;
+let exComparacao = (g === h);
+console.log(exComparacao);
+
+
