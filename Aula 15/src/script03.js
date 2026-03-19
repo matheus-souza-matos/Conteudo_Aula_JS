c7a8c4421d14fb330db52cc6685a1c4fd7055a0a
Matheus Matos <matheusmatos463463@gmail.com>
2026-03-19 20:24:02 -0300

    Aula 16 - Alterando o Estilo de Elementos com o DOM

--- Aula 15/src/script03.js
+++ Aula 15/src/script03.js
@@ -7,3 +7,10 @@ elemento1.innerHTML = "Somente a dor coletiva gera união.";
 // alterando atributos
 const elemento2 = document.querySelector(".foto");
 elemento2.src = "./img/img.png";
+
+elemento.width = 400;
+
+// Conteúdo dinâmico (data)
+const elemento2 = document.querySelector("#ex2");
+
+elemento2.innerHTML = "Data atual: " + Date();
