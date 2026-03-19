92b6ed5c47d561366a021422f9b23faeb6b41014
Matheus Matos <matheusmatos463463@gmail.com>
2026-03-18 21:22:02 -0300

    Aula 15 - Estudando o DOM

---
+++ Aula 15/src/script02.js
@@ -0,0 +1,27 @@
+// utilizando o getElementById()
+const element1 = document.getElementById("selecionandoElemento");
+console.log(element1);
+
+
+// utilizando o getElementByTagName
+const elemento2 = document.getElementsByTagName("h1");
+console.log(elemento2[1]);
+
+
+// utilizando o getElementsByClassName()
+const elemento3 = document.getElementsByClassName("testeClasse");
+console.log(elemento3)
+
+
+// utilizando o getQuerySelector
+const elemento4 = document.querySelector(".querySelector");
+console.log(elemento4);
+const elemento5 = document.querySelector("#querySelector");
+console.log(elemento4);
+
+
+// utilizando o getQuerySelectorAll
+const elementos6 = document.querySelectorAll("h3")
+console.log(elementos6);
+
+// 
