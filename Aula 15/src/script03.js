ed54c41a7d0ac17ce569668a0c98b97041eda504
Matheus Matos <matheusmatos463463@gmail.com>
2026-03-18 22:10:25 -0300

    Aula 15 - Estudando Manipulações de Elementos no HTML

---
+++ Aula 15/src/script03.js
@@ -0,0 +1,9 @@
+// utilizando o innerHTML
+const elemento1 = document.querySelector(".mudandoElemento");
+
+elemento1.innerHTML = "Somente a dor coletiva gera união.";
+
+
+// alterando atributos
+const elemento2 = document.querySelector(".foto");
+elemento2.src = "./img/img.png";
